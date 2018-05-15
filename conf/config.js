2949bcbe32d15753935ffa7e668e02357517f8d2
kinzaR <rian.kinza@gmail.com>
2018-05-15 13:38:09 +0200

    use of cba-elements 1.8.1

--- conf/config.js
+++ conf/config.js
@@ -2,16 +2,16 @@
  * This is a configuration file.
  * Changes to this file may cause the application does not work as it should
  **/
-// STEVIA_MANAGER_LOG = true;
-//STEVIA_SERVER_HOST = '//ws.babelomics.org/stv01';
-//STEVIA_SERVER_HOST = 'http://ws2.babelomics.org/stevia_cellmaps_hipathia_pathact_metabolizer';
-STEVIA_SERVER_HOST ='http://stevia-curso.clinbioinfosspa.es/slurmia01';
-//STEVIA_SERVER_HOST = '//ws2.babelomics.org/stevia_cellmaps_hipathia_pathact_metabolizer';
-// STEVIA_SERVER_HOST = '//ws.babelomics.org/teststv01';
-// STEVIA_SERVER_HOST = '//localhost:5555';
+// CBA_MANAGER_LOG = true;
 
-// STEVIA_SHOW_JOB_ERROR_FILES = false;
-STEVIA_SHOW_JOB_ERROR_FILES = true;
+CBA_SERVER_HOST ='http://stevia-curso.clinbioinfosspa.es/slurmia01';
+// CBA_SERVER_HOST = '//ws.babelomics.org/teststv01';
+// CBA_SERVER_HOST = '//localhost:5555';
+
+// CBA_SHOW_JOB_ERROR_FILES = false;
+CBA_SHOW_JOB_ERROR_FILES = true;
+
 UPLOAD_FILES_WITH_ERRORS = true;
-// STEVIA_SHOW_HIDDEN_FILES = false;
-STEVIA_SHOW_HIDDEN_FILES = true;
+
+// CBA_SHOW_HIDDEN_FILES = false;
+CBA_SHOW_HIDDEN_FILES = true;
