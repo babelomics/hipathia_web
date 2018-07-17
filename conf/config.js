2b1943c1b6fa6d07ed96bd1fd991f7b43c6c3586
kinzaR <rian.kinza@gmail.com>
2018-07-17 17:57:27 +0200

    tabs and interaction between table and PathwauViewer

--- conf/config.js
+++ conf/config.js
@@ -3,15 +3,11 @@
  * Changes to this file may cause the application does not work as it should
  **/
 // CBA_MANAGER_LOG = true;
-
 CBA_SERVER_HOST ='http://stevia-curso.clinbioinfosspa.es/slurmia01';
-// CBA_SERVER_HOST = '//ws.babelomics.org/teststv01';
 // CBA_SERVER_HOST = '//localhost:5555';
 
 // CBA_SHOW_JOB_ERROR_FILES = false;
 CBA_SHOW_JOB_ERROR_FILES = true;
-
 UPLOAD_FILES_WITH_ERRORS = true;
-
 // CBA_SHOW_HIDDEN_FILES = false;
 CBA_SHOW_HIDDEN_FILES = true;
