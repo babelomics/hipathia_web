8bbb861e8e6a2a6d93fc8fbd9ef12eed41e0ad80
kinzaR <rian.kinza@gmail.com>
2018-10-18 18:03:28 +0200

    change SHOW JOB ERROR FILES to false

--- conf/config.js
+++ conf/config.js
@@ -6,8 +6,8 @@
 CBA_SERVER_HOST ='http://stevia-curso.clinbioinfosspa.es/slurmia01';
 // CBA_SERVER_HOST = '//localhost:5555';
 
-// CBA_SHOW_JOB_ERROR_FILES = false;
-CBA_SHOW_JOB_ERROR_FILES = true;
+CBA_SHOW_JOB_ERROR_FILES = false;
+//CBA_SHOW_JOB_ERROR_FILES = true;
 UPLOAD_FILES_WITH_ERRORS = true;
 // CBA_SHOW_HIDDEN_FILES = false;
 CBA_SHOW_HIDDEN_FILES = true;
